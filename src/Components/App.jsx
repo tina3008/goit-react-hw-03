2ef77008366c4eca7b7487b747869238fa33770f
tina3008 <kicja_3@ukr.net>
2024-05-11 18:59:25 +0300

    3task

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-
+ import { useState } from 'react'
+ import startContacts from '../Components/contact.json'
+ import ContactForm from "./ContactForm/ContactForm";
+ import SearchBox from "./SearchBox/SearchBox";
+ import ContactList from "./ContactList/ContactList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[contacts, setContacts] = useState(startContacts);
+  const [filter, setFilter] = useState('');
+
+
+  const onDelete = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) =>
+   contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+ 
+  const addContact =(newContact)=>{
+    setContacts((prevContact)=>{
+      return[...prevContact, newContact]
+    })
+  }
 
   return (
     <>
-      
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={addContact} />
+        <SearchBox value={filter} onFilter={setFilter}/>       
+        <ContactList contacts={visibleContacts}  onDelete={onDelete}/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
